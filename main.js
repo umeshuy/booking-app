d46953fb39e3eb02175ef2e2da5ed76459f72353
umeshuy <110802360+umeshuy@users.noreply.github.com>
2022-12-21 11:11:30 +0530

    booking app

---
+++ main.js
@@ -0,0 +1,45 @@
+function saveToLocalStorage(event) {
+    event.preventDefault();
+    const name = event.target.username.value;
+    const email = event.target.emailId.value;
+    const phonenumber = event.target.phonenumber.value;
+    // localStorage.setItem('name', name);
+    // localStorage.setItem('email', email);
+    // localStorage.setItem('phonenumber', phonenumber)
+    const obj = {
+        name,
+        email,
+        phonenumber
+    }
+    localStorage.setItem(obj.email, JSON.stringify(obj))
+    showListofRegisteredUser(obj)
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    Object.keys(localStorage).forEach(key => {
+        const user = JSON.parse(localStorage.getItem(key))
+        showListofRegisteredUser(user)
+    })
+})
+
+function showListofRegisteredUser(user){
+    const parentNode = document.getElementById('listOfitems');
+    const createNewUserHtml = `<li id='${user.email}'>${user.name} - ${user.email} - ${user.phonenumber}
+                                    <button onclick=deleteUser('${user.email}')>Delete</button>
+                                </li>
+                                `
+    console.log(createNewUserHtml)
+    parentNode.innerHTML +=  createNewUserHtml;
+    console.log(parentNode.innerHTML)
+}
+
+function deleteUser(email) {
+    localStorage.removeItem(email)
+    removeItemFromScreen(email)
+}
+
+function removeItemFromScreen(email){
+    const parentNode = document.getElementById('listOfitems');
+    const elem = document.getElementById(email)
+    parentNode.removeChild(elem);
+}
